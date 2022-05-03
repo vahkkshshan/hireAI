fd3ffcd994b4f33805a26b0dc9502771d4fa266e
vahkkshshan <vahkkshshan@gmail.com>
2022-05-03 19:47:16 +0530

    fixed app.py update issue login fixed

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import json
 import os
-from fastapi import FastAPI, Body, HTTPException, status, Form, UploadFile, File,Query
+from fastapi import FastAPI, Body, HTTPException, status, Form, UploadFile, File, Query, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, EmailStr, SecretStr, constr
 from bson import ObjectId
-from typing import Optional, List,OrderedDict
+from typing import Optional, List, OrderedDict
 from pymongo import MongoClient
 from awsConnector import upload_file_to_bucket
 import motor.motor_asyncio
+from hashing import Hash
+from jwttoken import create_access_token
+from oauth import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # client = MongoClient()
 # client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 # db = client.college
@@ -38,12 +54,11 @@ class PyObjectId(ObjectId):
         field_schema.update(type="string")
 
 
-class StudentModel(BaseModel):
+class UserModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: str = Field(...)
-    email: EmailStr = Field(...)
-    course: str = Field(...)
-    gpa: float = Field(..., le=4.0)
+    username: str = Field(...)
+    company: str = Field(...)
+    password: str = Field(...)
 
     class Config:
         allow_population_by_field_name = True
@@ -51,12 +66,25 @@ class StudentModel(BaseModel):
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-                "course": "Experiments, Science, and Fashion in Nanophotonics",
-                "gpa": "3.0",
+                "username": "Jane Doe",
+                "company": "jdoe@example.com",
+                "password": "Software Engineer"
             }
         }
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
 class InterviewModel(BaseModel):
@@ -124,13 +152,15 @@ class UpdateCandidateModel(BaseModel):
         }
 
 
-class UpdateStudentModel(BaseModel):
-    name: Optional[str]
-    email: Optional[EmailStr]
-    course: Optional[str]
-    gpa: Optional[float]
+class StudentModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    course: str = Field(...)
+    gpa: float = Field(..., le=4.0)
 
     class Config:
+        allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
@@ -143,17 +173,55 @@ class UpdateStudentModel(BaseModel):
         }
 
 
-# @app.post("/", response_description="Add new student", response_model=StudentModel)
-# async def create_student(student: StudentModel = Body(...)):
-#     student = jsonable_encoder(student)
-#     new_student = await db["students"].insert_one(student)
-#     created_student = await db["students"].find_one({"_id": new_student.inserted_id})
-#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+@app.post("/st", response_description="Add new student", response_model=StudentModel)
+async def create_student(student: StudentModel = Body(...)):
+    student = jsonable_encoder(student)
+    new_student = await db["students"].insert_one(student)
+    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+
+
+@app.get("/")
+def read_root(current_user: UserModel = Depends(get_current_user)):
+    return {"data": "Hello OWrld"}
+
+
+@app.post('/register', response_description="Add new user", response_model=UserModel)
+async def create_user(username: str = Form(None), company: str = Form(None), password: str = Form(None)):
+    # print(request.password)
+    # print(request)
+    # print(request.password)
+    hashed_pass = Hash.bcrypt(password)
+    # user_object = dict(request)
+    user = UserModel(username=username, company=company, password=hashed_pass)
+    user = jsonable_encoder(user)
+    # print("hi dude")
+    # print(user_object["password"])
+    # user_object["password"] = hashed_pass
+    # print(user_object["password"])
+
+    user_id = await db["users"].insert_one(user)
+    created_user= await db["users"].find_one({"_id": user_id.inserted_id})
+    # print(user)
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
+
+
+@app.post('/login')
+async def login(request: OAuth2PasswordRequestForm = Depends()):
+    user = db["users"].find_one({"username": request.username})
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No user found with this {request.username} username')
+    if not Hash.verify(user["password"], request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Wrong Username or password')
+    access_token = create_access_token(data={"username": user["username"]})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.post("/candidate", response_description="Add new candidate", response_model=CandidateModel)
 async def create_candidate(name: str = Form(None), email: EmailStr = Form(None), position: str = Form(None),
-                           cv: UploadFile = File(...), interview_name: str = Query(None, enum=list(db["interview"].find()))):
+                           cv: UploadFile = File(...),
+                           interview_name: str = Query(None, enum=list(db["interview"].find()))):
     print("hey")
     ins = list(db["interview"].find())
     print(ins)
@@ -175,7 +243,7 @@ async def create_candidate(name: str = Form(None), email: EmailStr = Form(None),
     #         candidate = jsonable_encoder(candidate)
     #         new_candidate = await db["candidate"].insert_one(candidate)
     #         created_candidate = await db["candidate"].find_one({"_id": new_candidate.inserted_id})
-        #     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_candidate)
+    #     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_candidate)
 
 
 @app.post("/interview", response_description="Add new interview", response_model=InterviewModel)
@@ -234,87 +302,3 @@ async def delete_candidate(id: str):
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"Cadidate {id} not found")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.get(
-#     "/", response_description="List all students", response_model=List[StudentModel]
-# )
-# async def list_students():
-#     students = await db["students"].find().to_list(1000)
-#     return students
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.get(
-#     "/{id}", response_description="Get a single student", response_model=StudentModel
-# )
-# async def show_student(id: str):
-#     if (student := await db["students"].find_one({"_id": id})) is not None:
-#         return student
-#
-#     raise HTTPException(status_code=404, detail=f"Student {id} not found")
-#
-#
-# @app.put("/{id}", response_description="Update a student", response_model=StudentModel)
-# async def update_student(id: str, student: UpdateStudentModel = Body(...)):
-#     student = {k: v for k, v in student.dict().items() if v is not None}
-#
-#     if len(student) >= 1:
-#         update_result = await db["students"].update_one({"_id": id}, {"$set": student})
-#
-#         if update_result.modified_count == 1:
-#             if (
-#                     updated_student := await db["students"].find_one({"_id": id})
-#             ) is not None:
-#                 return updated_student
-#
-#     if (existing_student := await db["students"].find_one({"_id": id})) is not None:
-#         return existing_student
-#
-#     raise HTTPException(status_code=404, detail=f"Student {id} not found")
-#
-#
-# @app.delete("/{id}", response_description="Delete a student")
-# async def delete_student(id: str):
-#     delete_result = await db["students"].delete_one({"_id": id})
-#
-#     if delete_result.deleted_count == 1:
-#         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
-#
-#     raise HTTPException(status_code=404, detail=f"Student {id} not found")
