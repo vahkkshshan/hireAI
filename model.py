8e559109bd1898cd69bd1c1885bf80857ebc7c83
vahkkshshan <vahkkshshan@gmail.com>
2022-05-20 00:57:14 +0530

    added background tasks and threshold

--- model.py
+++ model.py
@@ -13,10 +13,10 @@ def predict(filename):
     df = df.iloc[:, 1:8]
     df = df * 100
     df_mean = df.mean(axis=0).round(0).astype('int')
-    df_positive=((df['happy']+df['surprise']+df['neutral'])/3).round(0)
-    df_negative=((df['fear']+df['sad']+df['disgust'])/3).round(0)
+    df_positive = ((df['happy'] + df['surprise'] + df['neutral']) / 3).round(0)
+    df_negative = ((df['fear'] + df['sad'] + df['disgust']) / 3).round(0)
 
-    df_pos_neg=((df_positive-df_negative)/df_positive)*100
+    df_pos_neg = (((df_positive - df_negative) / df_positive) * 100).round(0).astype('int')
 
-
-    return df_mean['angry'], df_mean['disgust'], df_mean['fear'], df_mean['happy'], df_mean['sad'], df_mean['surprise'], df_mean['neutral'],df_pos_neg
+    return df_mean['angry'], df_mean['disgust'], df_mean['fear'], df_mean['happy'], df_mean['sad'], df_mean['surprise'], \
+           df_mean['neutral'], df_pos_neg
